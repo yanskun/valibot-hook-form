eb43a4f6178988fca13894b4e7640cbe1a16d065
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-09-29 00:37:04 +0900

    top level `pipe`

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -3,23 +3,23 @@ import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { valibotResolver } from "@hookform/resolvers/valibot";
 import * as v from "valibot";
 
-enum GenderEnum {
-  female = "female",
-  male = "male",
-  other = "other",
-}
-
-const schema = v.object({
-  name: v.pipe(
-    v.object({
+const schema = v.pipe(
+  v.object({
+    name: v.object({
       firstName: v.string(),
       lastName: v.string(),
     }),
-    v.transform(({ firstName, lastName }) => `${firstName} ${lastName}`)
-  ),
-  isJapanese: v.boolean(),
-  gender: v.enum(GenderEnum),
-});
+    isJapanese: v.boolean(),
+  }),
+  v.transform((input) => {
+    const { firstName, lastName } = input.name;
+    return {
+      name: input.isJapanese
+        ? `${lastName} ${firstName}`
+        : `${firstName} ${lastName}`,
+    };
+  })
+);
 
 type IFormInput = v.InferInput<typeof schema>;
 
@@ -81,19 +81,6 @@ export function Form() {
             </label>
           </div>
 
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Gender Selection
-            </label>
-            <select
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              {...register("gender")}
-            >
-              <option value="female">female</option>
-              <option value="male">male</option>
-              <option value="other">other</option>
-            </select>
-          </div>
           <button
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             type="submit"
